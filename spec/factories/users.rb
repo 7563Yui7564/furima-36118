6367077388f580fda2e77ec1dc9f85158a8517f9
7563Yui7564 <takashi.t1001@gmail.com>
2021-07-23 00:30:40 +0900

    モデル単体テストコード-1

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,10 +3,10 @@ FactoryBot.define do
     gimei = Gimei.name
     nickname              { Faker::Name.initials }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 5) }
     password_confirmation { password }
-    last_name             { Faker::Name.last_name }
-    first_name            { Faker::Name.first_name }
+    last_name             { gimei.last.kanji }
+    first_name            { gimei.first.kanji }
     last_name_kana        { gimei.last.katakana }
     first_name_kana       { gimei.first.katakana }
     birth_date            { Faker::Date.birthday }
