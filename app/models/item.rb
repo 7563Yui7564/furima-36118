db731eca8c4d93bec33ef3fb4b10b33e37ac1f05
7563Yui7564 <takashi.t1001@gmail.com>
2021-07-23 19:53:28 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,22 @@ class Item < ApplicationRecord
   belongs_to :delivery_burden
   belongs_to :delivery_day
   belongs_to :prefecture
-
   belongs_to :user
   has_one_attached :image
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    # 価格は、半角数字での入力が必須であること
+    validates :price, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters." }
+  end
+  with_options numericality: { other_than: 1, message: "can't be blanck"}
+    validates :category_id
+    validates :condition_id
+    validates :delivery_burden_id
+    validates :prefecture_id
+    validates :delivery_day_id
+  end
+   # 価格は、¥300~¥9,999,999の間のみ保存可能であること
+    validates :price, numericality: { less_than_or_equal_to: 300, greater_than_equal_to: 9999999, message: "is out of setting range." }
 end
