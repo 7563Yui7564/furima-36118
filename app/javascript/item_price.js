38385a6080cc6ef78d42022f412c1c845a4299da
7563Yui7564 <takashi.t1001@gmail.com>
2021-07-25 16:09:05 +0900

    商品出品機能修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,18 +2,13 @@ window.addEventListener('load', () => {
 
  const itemPrice = document.getElementById("item-price")
  itemPrice.addEventListener('input', () => {
-  const inputValue = itemPrice.value
-  console.log(inputValue)
-  
+  const inputValue = itemPrice.value  
 
  const addTaxPrice = document.getElementById("add-tax-price")
  addTaxPrice.innerHTML = (Math.floor(inputValue * 0.1))
- console.log(addTaxPrice)
 
  const profit = document.getElementById("profit")
- const price_result = inputValue * 0.1
- console.log(price_result)
+ const price_result = (Math.floor(inputValue * 0.1))
  profit.innerHTML = (Math.floor(inputValue - price_result))
- console.log(profit)
  }) 
 })
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,81 +4,97 @@ RSpec.describe Item, type: :model do
     @item = FactoryBot.build(:item)
   end
 
-  describe '出品の保存' do
-    it '全て入力すれば保存ができる' do
+  describe '出品機能' do
+    context '出品ができるとき' do
+      it '全て入力すれば保存ができる' do
       expect(@item).to be_valid
+      end
     end
 
-    it 'nameが空では保存できない' do
-      @item.name = nil
+    context '出品ができないとき' do
+      it 'nameが空では保存できない' do
+      @item.name = ' '
       @item.valid?
       expect(@item.errors.full_messages).to include("Name can't be blank")
-    end
+      end
 
-    it 'imageが空では保存できない' do
+      it 'imageが空では保存できない' do
       @item.image = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Image can't be blank")
-    end
+      end
 
-    it 'descriptionが空では保存できない' do
-      @item.description = nil
+      it 'descriptionが空では保存できない' do
+      @item.description = ' '
       @item.valid?
       expect(@item.errors.full_messages).to include("Description can't be blank")
-    end
+      end
 
-    it 'categoryを選択していないと保存できない' do
+      it 'categoryを選択していないと保存できない' do
       @item.category_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Category can't be blank")
-    end
+      end
 
-    it 'Conditionが選択されていないと保存できない' do
+      it 'Conditionが選択されていないと保存できない' do
       @item.condition_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Condition can't be blank")
-    end
+      end
 
-    it 'delivery_burdenが選択されていないと保存できない' do
+      it 'delivery_burdenが選択されていないと保存できない' do
       @item.delivery_burden_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Delivery burden can't be blank")
-    end
+      end
 
-    it 'prefectureが選択されていないと保存できない' do
+      it 'prefectureが選択されていないと保存できない' do
       @item.prefecture_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Prefecture can't be blank")
-    end
+      end
 
-    it 'delivery_dayが選択されていないと保存できない' do
+      it 'delivery_dayが選択されていないと保存できない' do
       @item.delivery_day_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Delivery day can't be blank")
-    end
+      end
 
-    it 'priceが選択されていないと保存できない' do
-      @item.price = nil
+      it 'priceが空では保存できない' do
+      @item.price = ' '
       @item.valid?
       expect(@item.errors.full_messages).to include("Price can't be blank")
-    end
+      end
 
-    it 'priceが全角数字では保存できない' do
+      it 'priceが全角数字では保存できない' do
       @item.price = '３００'
       @item.valid?
       expect(@item.errors.full_messages).to include('Price is invalid. Input half-width characters.')
-    end
+      end
 
-    it 'priceが300円未満では保存できない' do
+      it 'priceが半角英数混合では保存できない' do
+      @item.price = '300y'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price is invalid. Input half-width characters.')
+      end  
+
+      it 'priceが半角英語では保存できない' do
+      @item.price = 'yui'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price is invalid. Input half-width characters.')
+      end  
+  
+      it 'priceが300円未満では保存できない' do
       @item.price = '299'
       @item.valid?
       expect(@item.errors.full_messages).to include('Price is out of setting range.')
-    end
+      end
 
-    it 'priceが10,000,000円未満では保存できない' do
+      it 'priceが10,000,000円以上では保存できない' do
       @item.price = '10000000'
       @item.valid?
       expect(@item.errors.full_messages).to include('Price is out of setting range.')
-    end
+      end
+    end  
   end
 end
