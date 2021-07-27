db350cb1fcc5237d19f0be17361b6bb423e9e09e
7563Yui7564 <takashi.t1001@gmail.com>
2021-07-27 18:35:10 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :set_item, except: [:index, :new, :create]
+  before_action :set_item, except: [:index, :create, :new]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :contributor_confirmation, only: [:edit]
+  before_action :contributor_confirmation, only: [:edit :delete]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -35,6 +35,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
+  end  
+  
   private
 
   def contributor_confirmation
