1895ef86ff52a179bcbbeea4fa0cab7edcc8ccb8
7563Yui7564 <takashi.t1001@gmail.com>
2021-07-31 18:43:17 +0900

    sold outの表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,14 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:index, :create]
+  before_action :authenticate_user!, only: :index
+  before_action :contributor_confirmation, only: :index
 
   def index
     @order_address = OrderAddress.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
-    @item = Item.find(params[:item_id])
     if @order_address.valid?
        pay_item
        @order_address.save
@@ -18,6 +19,16 @@ class OrdersController < ApplicationController
   end
 
   private
+
+  def set_order
+    @item = Item.find(params[:item_id])
+  end
+
+  def contributor_confirmation
+    if current_user == @item.user || @item.order.present?
+    redirect_to root_path 
+    end
+  end
 
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
